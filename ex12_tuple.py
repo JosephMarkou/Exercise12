026252d671a57be3a5bf467ebb2c43e396d88207
JosephMarkou <joseph.markou@sky.uk>
2023-03-06 19:05:44 +0000

    Comment update to ex12_tuple.py

--- ex12_tuple.py
+++ ex12_tuple.py
@@ -4,4 +4,4 @@ tup2 = 'Hello',
 print(len(tup2))
 
 # tup1 is a String with 5 characters
-# tup2 is a Tuple because of the trailing comma - however it is not good practice to create tuples without a parentheses
+# tup2 is a Tuple because of the trailing comma - however it is not good practice to create tuples without a parentheses ()
